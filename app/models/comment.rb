628b290fb825818a68cd41018d7ff70026321fca
Rohan James <rohanjames9282@gmail.com>
2023-01-21 18:25:50 +0530

    Add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  validates :comment, presence: true
+  validates :user, presence: true
+  validates :post, presence: true
 end
