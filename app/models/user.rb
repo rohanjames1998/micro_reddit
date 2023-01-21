5c9a9dc16e3a792c3b9d32571ea2271bc5055d1e
Rohan James <rohanjames9282@gmail.com>
2023-01-21 18:33:36 +0530

    Add comment association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, length: { in: 5..10 }, presence: true, uniqueness: true
   validates :password, length: { in: 6..16 }, presence:true
   validates :email, presence: true
