7619b6f082c5ef6ee9fbb3640a5fbb1fa0e76f3a
Rohan James <rohanjames9282@gmail.com>
2023-01-21 15:53:42 +0530

    Add schema for posts

--- db/migrate/20230121100035_create_posts.rb
+++ db/migrate/20230121100035_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.text :post
+      t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps
     end
